7a12571f9909a24536a0ed2db7f95108fc7e3d82
charlie-kaiser <64374984+charlie-kaiser@users.noreply.github.com>
2026-04-20 16:48:50 +0200

    Solve import Gemini bug 4

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ Devuelve exactamente:
 }
 `;
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
         const result = await model.generateContent([
             prompt,
             {
